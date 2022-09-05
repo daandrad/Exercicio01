93ba0e0fb5b407327acf19b73d55955b4cb0fe4c
Daniel Andrade <46009616+daandrad@users.noreply.github.com>
2022-09-05 22:06:51 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+      String nomeProduto;
+      float precoCusto;
+      float precoVenda;
+        
+      float totalCusto = 0.0f;
+      float totalVenda = 0.0f;
+        
+      Scanner leitor = new Scanner(System.in);
+      int i = 0;
+      for (;i < 3; i++){
+        System.out.println("Digite o nome do produto");
+          nomeProduto = leitor.nextLine();
+            
+          System.out.println("Digite o preço de custo do produto");
+          precoCusto = leitor.nextFloat();
+            
+          System.out.println("Digite o preço de venda do produto");
+          precoVenda = leitor.nextFloat();
+            
+          totalCusto = totalCusto + precoCusto;
+          totalVenda = totalVenda + precoVenda;
+            
+          if(precoCusto == precoVenda){
+           System.out.println("Houve um empate entre o preco de custo e venda");
+          }else{
+            if(precoCusto > precoVenda){
+              System.out.println("Prejuizo");
+                }else{
+                  System.out.println("Lucro");
+                }
+               
+            }
+            
+          System.out.println(nomeProduto + ", preço de custo = " + precoCusto + ", preço de venda = " + precoVenda);
+            
+        }
+          System.out.println("A media do preço de custo é de: " + (totalCusto/ i));
+          System.out.println("A media do preço de venda é de: " + (totalVenda/ i));      
+                
+        
+    }
+}
